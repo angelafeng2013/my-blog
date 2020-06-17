2547a029095adef45dd3dfc625d249332aeeb435
Angela Pan <angelapan@Angelas-MacBook-Air.local>
2020-06-17 11:29:36 -0700

    first commit

---
+++ src/build/precache-manifest.a8c0f6570d10d68bd4cfe97c64313848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037da27dd6b339d04ae594e501e39826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27086db58f7c8a1c6a1b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "32392d509277c021265f",
+    "url": "/static/js/2.01c38188.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.01c38188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27086db58f7c8a1c6a1b",
+    "url": "/static/js/main.2db6be2c.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
